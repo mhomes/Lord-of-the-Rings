0df33ba10cedee0ba1c6c68c0e37208d8b65ad24
MatthewHomes <mhomes@butler.edu>
2019-03-18 20:44:26 -0400

    Update pmerge.cpp
    worked on pmerge running into seg fault

--- Project3/pmerge.cpp
+++ Project3/pmerge.cpp
@@ -16,7 +16,6 @@ Project by: Allen Burris and Mathew Homes
 // mpirun -q -np 32 blah
 
 using namespace std;
-
 int rank(int *a, int first, int last, int ValToFind) {
 
 	if (a[first + (last - first) / 2] == ValToFind)
@@ -51,6 +50,50 @@ void smerge(int *a, int f1, int l1, int f2, int l2) {
 	delete[] hold;
 }
 
+void pmerge(int * a , int * b, int first , int last, int mid, int my_rank, int p){
+	int n = (last - first + 1);
+	int sampleSize = log2(n / 2);
+	int* localSRankA = new  int [sampleSize];
+	int* localSRankB = new  int [sampleSize];
+	int* SRankA = new  int [sampleSize];
+	int* SRankB = new  int [sampleSize];
+ 
+ 
+	for (int x = 0; x < sampleSize; x++){
+		localSRankA[x]= 0;
+		localSRankB[x]= 0;
+		SRankA[x]= 0;
+		SRankB[x]= 0;
+	}
+	
+	
+	for (int i = sampleSize * my_rank; i < n; i += sampleSize*p){
+		int j = my_rank;
+		localSRankA[j] = rank(&a[mid+1], mid + 1, last, a[0+j]);
+		localSRankB[j] = rank(&a[0], first, mid, a[mid+1+j]);
+		j+=p;
+	}
+	
+	MPI_Allreduce(&localSRankA, &SRankA, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+	MPI_Allreduce(&localSRankB, &SRankB, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+	
+	for (int x = 0; x< sampleSize ; x++){
+		cout << SRankA[x]<< endl;
+		cout << SRankB[x]<< endl;
+	}
+}		
+
+void mergesort(int *a, int first, int last, int my_rank, int p) {
+	if ((last - first ) <= 8) return; //(last <= first) return;
+	if (last == first + 1) {
+		if (a[first] < a[last]) swap(a[first], a[last]);
+		return; }
+	int mid = (first + last) / 2;
+	mergesort(a, first, mid, my_rank, p);
+	mergesort(a, mid + 1, last, my_rank, p);
+	int * b = new int[last - first + 1];
+	pmerge(a, b, first, mid, last, my_rank, p); 
+	}
 
 int main(int argc, char * argv[]) {
 
@@ -78,34 +121,51 @@ int main(int argc, char * argv[]) {
 
 	/* Here is the parrallel version for when smerge and rank are working.
 	if (my_rank == 0) {
-
 		cout << "Welcome to the pMerge Program. \n Please enter the size of the array you wish to sort:" << endl;
 		cin >> n;
-
 		srand(time(0));
 		input = new int[n];
-
 		cout << endl << "The unsorted array: " << endl;
-
 		for (int x = 0; x < n; x++) {
 			input[x] = rand() % 100000;
 			cout << input[x] << " ";
 		}
 		cout << endl;
 	}
-
 	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
 	if (my_rank != 0)
 		input = new int[n];
-
 	MPI_Bcast(input, n, MPI_INT, 0, MPI_COMM_WORLD);
-
 	*/
-
+if (my_rank= 0) {
 	int *a = new int[16];
-	int b[16] = { 1,3,5,7,9,11,13,15,2,4,6,8,10,12,14,16 };
+	//a = { 1,3,5,7,9,11,13,15,2,4,6,8,10,12,14,16 };
+	n = 16;
+	for (int x = 0; x < n; x++)
+		a[x] = x+1;
+	MPI_Bcast(&a, n, MPI_INT, 0, MPI_COMM_WORLD);
+	
+	mergesort(a, 0, n-1, my_rank, p);
+	if (my_rank == 0)
+		for (int i = 0; i < 16; i++)
+			cout << a[i] <<endl;
 
+	cout << endl;
+}
+else{
+	int *a = new int[16];
+	n = 16;
+	MPI_Bcast(&a, n, MPI_INT, 0, MPI_COMM_WORLD);
+	
+	mergesort(a, 0, n-1, my_rank, p);
+	if (my_rank == 0)
+		for (int i = 0; i < 16; i++)
+			cout << a[i] <<endl;
+
+	cout << endl;
+
+}
+/*
 	int c[20] = { 1,2,4,5,6,7,9,10,12,14,15,17,20,21,40,50,55,56,59,60 };
 	int * d = new int[20];
 
@@ -118,26 +178,21 @@ int main(int argc, char * argv[]) {
 	for (int i = 0; i < 16; i++)
 		a[i] = b[i];
 	cout << endl;
+*/
+	
 
-	int log = log2(n / 2);
-
-	int * rankA = new int[log];
-	int * rankB = new int[log];
+	//int * rankA = new int[log];
+	//int * rankB = new int[log];
 
 
 
-	smerge(a, 0, 7, 8, 15);
-	/*if (my_rank == 0)
-		for (int i = 0; i < 16; i++)
-			cout << a[i] <<endl;*/
-
-	cout << endl;
+	
 
 	// Shut down MPI
 	MPI_Finalize();
 
-	delete[] input;
-	delete[] a;
+	//delete[] input;
+	//delete[] a;
 
 	return 0;
 }
